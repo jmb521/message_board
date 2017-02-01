98da72e8189e740c1f1993dcecdd81ff6aed6ab0
Jennifer Pazos <jmp521@gmail.com>
2017-02-01 15:18:50 -0600

    updated sessions controller, added helper methods to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :authorize, :logged_in?, :is_admin?, :is_moderator?
+  def current_user
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+
+  end
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def is_admin?
+    current_user.admin
+  end
+
+  def is_moderator?
+    current_user.moderator
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_one :profile
   has_many :posts
   has_many :comments, :through => :posts
+  enum role: [:user, :moderator, :admin]
 end
