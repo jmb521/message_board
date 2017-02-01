777c48a4d9912b81c97cea10e9f1df096b8b75a9
Jennifer Pazos <jmp521@gmail.com>
2017-02-01 14:59:16 -0600

    working on login/sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
 
-  def new
-
-  end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to posts_path
+    else
+      redirect_to login_path
+    end
+  end
 
+  def login
+    @user = User.new
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   post "/sessions/create", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
