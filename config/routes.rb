7e0bb27f222487fd45e422a7c5cfafd560c4e8cb
Jennifer Pazos <jmp521@gmail.com>
2017-02-01 14:24:01 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   resources :posts
   resources :profiles
   resources :users
+  root 'application#home'
+
+  get '/signup', to: "users#create"
+  get "/login", to: "sessions#login"
+  post '/login' => 'sessions#create'
+  post "/sessions/create", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
