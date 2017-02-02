46dcc3a1fecbae74cb712278f830a2a8ef7b9097
Jennifer Pazos <jmp521@gmail.com>
2017-02-02 15:25:29 -0600

    setting up comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,34 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+    @user = User.find_by(id: params[:id])
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      redirect_to posts_path, notice: "Your comment couldn't be saved"
+    end
+  end
+
+  def edit
+    @comment = Comment.find_by(id: params[:id])
+    binding.pry
+  end
+
+  def update
+    @comment = Comment.find_by(id: params[:id])
+    @comment.update(comment_params)
+    redirect_to post_path(@comment.post_id)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :post_id, :content)
+  end
 end
