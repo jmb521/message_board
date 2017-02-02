8aaa31a38a06d8637e394642e9143d8ad6a9bfbd
Jennifer Pazos <jmp521@gmail.com>
2017-02-02 01:10:00 -0600

    started working on posts controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ before_action :authenticate_user, only:[:show, :edit]
       if @user.save
         session[:user_id] = @user.id
 
-        redirect_to user_path(@user)
+        redirect_to posts_path
       else
         render 'new'
       end
