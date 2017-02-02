44ceb90289815881f791c221c9d1d5eb51fc1a54
Jennifer Pazos <jmp521@gmail.com>
2017-02-02 01:26:07 -0600

    updated the index and show pages for basic view of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      redirect_to new_post_path
+    end
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
