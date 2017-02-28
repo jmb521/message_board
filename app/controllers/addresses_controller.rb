6d87dc50cbe126a7a39e03b1aa5c422e2211f433
Jennifer Pazos <jmp521@gmail.com>
2017-02-28 17:38:35 -0600

    refactored addresses_controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,12 +1,10 @@
 class AddressesController < ApplicationController
-  before_action :authorize
+  before_action :authorize, :set_address
   def new
-
     @address = Address.new(profile_id: params[:profile_id])
   end
 
   def show
-    @address = Address.find_by(id: params[:id])
   end
 
   def create
@@ -16,17 +14,12 @@ class AddressesController < ApplicationController
 
       redirect_to profile_address_path(@address)
     end
-
-
   end
 
   def edit
-    @address = Address.find_by(id: params[:id])
-
   end
 
   def update
-    @address = Address.find_by(id: params[:id])
     @address.update(address_params)
     binding.pry
     redirect_to profile_address_path(@address.profile_id, @address.user_id)
@@ -37,5 +30,9 @@ class AddressesController < ApplicationController
 
     def address_params
       params.require(:address).permit(:address1, :address2, :city, :state, :zipcode, :user_id, :profile_id)
+    end
+
+    def set_address
+      @address = Address.find_by(id: params[:id])
     end
 end
