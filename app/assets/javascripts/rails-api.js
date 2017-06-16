ba9a2f72f9bfd25d48e4b907654147806739b243
Jennifer Pazos <jmp521@gmail.com>
2017-06-16 13:33:01 -0500

    working on loading next post on post show page with button and ajax

--- app/assets/javascripts/rails-api.js
+++ app/assets/javascripts/rails-api.js
@@ -5,7 +5,10 @@ $(document).ready(function() {
 
 });
 
-  function Post(id, content, user_id, created_at, updated_at) {
+var allPosts = []; //a way to store the newly created post objects
+var allUsers = [];
+
+  function Post(id, content, user_id, created_at, updated_at, comments) {
     this.id = id;
     this.content = content;
     this.user_id = user_id;
@@ -33,6 +36,7 @@ $(document).ready(function() {
 
       for(var key in data) {
         post = new Post(data[key]["id"], data[key]["content"], data[key]["user_id"], data[key]["created_at"], data[key]["updated_at"]);
+        allPosts.push(post);
         var splitData = post["created_at"].split("T");
         var splitDate = splitData[0].split("-");
         splitDate = splitDate.join(",");
@@ -60,6 +64,7 @@ $(document).ready(function() {
         //loop
         for(var key in data) {
           user = new User(data[key]["id"], data[key]["username"]);
+          allUsers.push(user);
         for(var i=0; i < userIdsArray.length; i++) {
 
           //  for(var j=0; j< data.length; j++) {
@@ -82,18 +87,47 @@ $(document).ready(function() {
 
   /////////////////////////////////////////////////////////////////////////////
 //use jquery/api to get the next post and display it to the user.
-
-
   function clickNextPost() {
     $("#next_button").on("click", function() {
+
       var id = $(this).attr("data-id");
-      getNextPost(id);
+
+      getNextPost(id); //passing in the id of current post
 
     });
   }
 
   function getNextPost(id) {
-    $.get(`/posts/${id}.json`, function(data) {
-      console.log(data);
-    })
+    // $.get("/posts.json", function(data) { //need to get all posts to find out which one is the next one in the listing of posts.
+    //   var post;
+    //
+    //   for(var i=0; i < data.length; i++) {
+    //     post = new Post(data[i]["id"], data[i]["content"], data[i]["user_id"], data[i]["created_at"], data[i]["updated_at"]);
+    //     if(post.id === id) {
+    //       // $("#comment").text("data[i+1]["content"]");
+    //
+    //     }
+    //   }
+    // })
+    console.log("this is the id: " + id);
+    var foundPost = findPost(2);
+    console.log("findPost value " + foundPost);
+
+    console.log("this is the post: " + foundPost);
+    $("#content").text(foundPost.content);
+
+    //since we already have the posts saved in a post object, I will use that to get the next post idea instead of making another request.
+
   }
+
+  function findPost(id) {
+    //find the post object by ID
+    for(var key in allPosts) {
+      if(allPosts[key]["id"] === id) {
+        // console.log(allPosts[key]["id"])
+        return allPosts[key];
+      }
+    }
+  }
+  console.log(allPosts);
+  console.log(allUsers);
